915b2e4a4cf8c05d37db9f3517aa995df720bb23
seora <seora@DESKTOP-RTPS6A2>
2022-05-27 17:44:02 +0900

    프로필 업데이뜌

--- src/main/java/com/viseo/controller/hsr/ProfileUpdate.java
+++ src/main/java/com/viseo/controller/hsr/ProfileUpdate.java
@@ -9,14 +9,15 @@ import javax.servlet.http.HttpSession;
 
 import com.viseo.controller.BlpInter;
 import com.viseo.dao.UpdateDao;
+import com.viseo.vo.UpdateVO;
 
 /**
  * 이 클래스는 회원 관련 데이터베이스 작업을 전담해서 처리하는 클래스
  * @author 한서라
- * @since 2022.05.12
+ * @since 2022.05.27
  * @version v.1.0
  * 		작업 이력)
- * 				2022.05.12	- 담당자 : 한서라
+ * 				2022.05.27	- 담당자 : 한서라
  * 							  내용  :
  * 						
  */
@@ -26,6 +27,25 @@ public class ProfileUpdate implements BlpInter {
 	@Override
 	public String exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String view = "/member/info/ProfileUpdate";
+		// 로그인 체크
+		
+		  HttpSession session = req.getSession(); 
+		  String sid = (String)session.getAttribute("SID"); 
+		  if(sid == null) { 
+		  view = "/member/loginForm.blp";
+		  req.setAttribute("isRedirect", true); 
+		  return view; 
+		  }
+		
+		  // 데이터베이스에서 내정보 꺼내오고 
+		  UpdateDao uDAO = new UpdateDao(); 
+		  UpdateVO fVO = uDAO.getIdInfo(sid); 
+		  
+		  //  UpdateVO fVO = uDAO.getProfile(sid); 
+		 
+		  // 데이터 심고 
+		  req.setAttribute("DATA", fVO);
+		
 		return view;
 	}
 
