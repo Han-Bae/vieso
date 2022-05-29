6314baf59e11ea455a797a9873030c240716552d
jan <samjan1127@gmail.com>
2022-05-29 14:42:19 +0900

    개발자 주석 추가

--- src/main/java/com/viseo/controller/jdb/LogoutProc.java
+++ src/main/java/com/viseo/controller/jdb/LogoutProc.java
@@ -1,5 +1,16 @@
 package com.viseo.controller.jdb;
 
+/**
+ * 
+ * @author	전다빈
+ * @since	2022.05.28
+ * @version	v.1.0
+ * 
+ * 			작업이력	]
+ * 				2022.05.28	-	담당자 : 전다빈
+ * 								내	용 : 로그아웃 처리 컨트롤러
+ */
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
